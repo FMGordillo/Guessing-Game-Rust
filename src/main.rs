cc1de44865e2f8209d029e159c868ae478c7e5fc
FMGordillo <facundomgordillo@gmail.com>
2022-05-23 05:58:05 +0200

    :sparkles: feat: added random number generator

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,17 @@
+use rand::prelude::*;
 
 fn main() {
     println!("Please, tell me your name first");
     let mut name = String::new();
-    // t adlet mut guess = String::new();
-    std::io::stdin().read_line(&mut name).expect("Failed to read line");
-    println!("Hello {}nice to meet you", name)
+    std::io::stdin()
+        .read_line(&mut name)
+        .expect("Failed to read line");
+
+    // TODO: Can we improve this?
+    let mut rng = rand::thread_rng(); // Necessary, I guess
+    let random_float: f64 = rng.gen(); // Float number between 0 and 1
+    let random: f64 = random_float.mul_add(10_f64, 0_f64); // Transform in to 1-10 range
+    let random_rounded: f64 = random.round(); // Round the result
+    println!("Hello {}nice to meet you", name);
+    println!("My number {}", random_rounded);
 }
