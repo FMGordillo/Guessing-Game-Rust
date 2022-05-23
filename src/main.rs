edea47ac4fa4151e55cbd006533456c4d6b5e414
FMGordillo <facundomgordillo@gmail.com>
2022-05-23 06:32:21 +0200

    :sparkles: feat: added one-try feature

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use rand::prelude::*;
+use std::cmp::Ordering;
 
 fn main() {
     println!("Please, tell me your name first");
@@ -10,8 +11,20 @@ fn main() {
     // TODO: Can we improve this?
     let mut rng = rand::thread_rng(); // Necessary, I guess
     let random_float: f64 = rng.gen(); // Float number between 0 and 1
-    let random: f64 = random_float.mul_add(10_f64, 0_f64); // Transform in to 1-10 range
-    let random_rounded: f64 = random.round(); // Round the result
+    let random: f64 = random_float.mul_add(100_f64, 0_f64); // Transform in to 1-100 range
+    let random_rounded = format!("{}", random.round()); // Round the result
     println!("Hello {}nice to meet you", name);
-    println!("My number {}", random_rounded);
+
+    println!("And now, tell me your guess");
+    let mut guess = String::new();
+    std::io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+    match guess.cmp(&random_rounded) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You've won!"),
+    }
+    println!("The secret {}, but your guess {}", random_rounded, guess);
 }
